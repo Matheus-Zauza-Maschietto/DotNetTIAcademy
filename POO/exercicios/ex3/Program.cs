c98287a3749d25abcb761c2f19d966aed2f17bb4
matheus zauza maschietto <matheus_maschieto@yahoo.com.br>
2022-12-06 14:39:00 -0300

    update

--- POO/exercicios/ex3/Program.cs
+++ POO/exercicios/ex3/Program.cs
@@ -1,6 +1,9 @@
 ﻿using ex3.ContaCorrente;
-class exercico3{
-    public static void Main(){
-        ContaCorrente conta = new ContaCorrente("Marcos", 124242, 4642-2, 34533.35);
-    }
+using System;
+
+class Programa
+{
+   
 }
+
+
