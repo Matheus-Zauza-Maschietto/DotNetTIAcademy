0cbfd8d7c61c748c31427db344156dba30003244
matheus zauza maschietto <matheus_maschieto@yahoo.com.br>
2022-12-15 11:11:59 -0300

    Update

---
+++ api/sistemaVendas/Context/VendasContext2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace sistemaVendas.Context
+{
+    public class VendasContext : DbContext
+    {
+        // public VendasContext(DbContextOptions<VendasContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ api/sistemaVendas/Models/Cliente.cs
@@ -0,0 +1,17 @@
+using System.Runtime.Intrinsics.X86;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistemaVendas.Models
+{
+    public class Cliente 
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
