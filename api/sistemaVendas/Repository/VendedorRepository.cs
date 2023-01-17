7b488c0f7d21e31b7b53617a91771ecc4cc14add
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-01-16 22:25:57 -0300

    update

--- api/sistemaVendas/Repository/VendedorRepository.cs
+++ api/sistemaVendas/Repository/VendedorRepository.cs
@@ -24,7 +24,6 @@ namespace sistemaVendas.Repository
         public Vendedor ObterPorId(int id){
             var vendedor = _context.Vendedores.Find(id);
             return vendedor;
-
         }
 
         public List<ObterVendedorDTO> ObterPorNome(string nome)
